cd78bbc5e784914ce0f46f7196396cf11f08594b
visharthigee <visharthigee@gmail.com>
2017-11-12 00:44:24 -0600

    Gets server info from CLI

---
+++ ClientSide.py
@@ -0,0 +1,25 @@
+import sys, pygame, mygui, clientThread, time, clientGame
+from pygame.locals import *
+from constants import *
+
+class PokerClient:
+
+    def __init__(self, screen):
+
+        
+   	print ("Client")
+    	serIP=raw_input("Enter IP: ")          #Enter IP and port on CLI
+	serPort=raw_input("Enter port: ")
+        print("Connecting")
+        if serIP == "1":
+           serIP = "10.42.0.45"
+        elif serIP == "2":
+           serIP = "172.24.136.242"
+
+        if serPort =="":
+           serPort = "1234"
+        cliObj = clientThread.ClientThread(str(serIP),int(serPort),screen)
+	begin = cliObj.sock.recv(1024)
+        if begin == "begin":
+           print "Unpaused"
+           clientGame.ClientGame(cliObj.sock, screen)
